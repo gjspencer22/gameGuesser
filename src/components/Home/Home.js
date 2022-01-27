facd7696dc09c9ee4f1b56c3646896950d5a3842
gjspencer22 <gjspencer22@gmail.com>
2022-01-27 01:29:18 -0700

    working on on button click begin quiz

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,7 @@ import './home.css';
 
 
 function Home() {
+    // const begin = begin();
     return (
         <div class = 'home'>
             <div class ='welcomeBox'>
@@ -10,14 +11,12 @@ function Home() {
                  <div>
                     <p class ='welcomeText'>Listen to a sound and guess which video game it is from!</p>
                 </div>
-                <button onclick = 'begin()' class ='beginButton' id = 'beginButton'>Begin Your Trial</button>
+                <button onclick = "begin()" class ='beginButton' id = 'beginButton'>Begin Your Trial</button>
             </div>
         </div>
     );
 }
 
-function begin () {
-    // const clicked = document.getElementById('beginButton')
-    console.log('oy')
-}
+
 export default Home;
+
