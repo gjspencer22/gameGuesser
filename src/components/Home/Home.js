30db24fe07bbcdef2aea33ef33e29ef0a9a56b5e
gjspencer22 <gjspencer22@gmail.com>
2022-03-03 20:42:43 -0700

    working on click function

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,35 +2,34 @@ import React from 'react';
 import './home.css';
 
 
+const beginButton = document.getElementById('beginButton')
+
+beginButton.addEventListener('click', begin)
+
+function begin() {
+    console.log('oychump')
+}
+
+
 function Home() {
-    // const begin = begin();
+    
     return (
-        <div className = 'home'>
-            <div className ='welcomeBox'>
-                    <h1 className ='welcomeHeader'> Welcome to Game Gueser! </h1>
-                 <div>
-                    <p className ='welcomeText'>Listen to a sound and guess which video game it is from!</p>
+        <div className='home'>
+            <div className='welcomeBox'>
+                <h1 className='welcomeHeader'> Welcome to Game Gueser! </h1>
+                <div>
+                    <p className='welcomeText'>Listen to a sound and guess which video game it is from!</p>
                 </div>
-<<<<<<< HEAD
-                <button className ='beginButton' id = 'beginButton' onClick = {begin()} >Begin Your Trial</button>
-=======
-                <button onclick = "begin()" class ='beginButton' id = 'beginButton'>Begin Your Trial</button>
->>>>>>> facd7696dc09c9ee4f1b56c3646896950d5a3842
+                {/* <button className ='beginButton' id = 'beginButton' onClick = {begin()} >Begin Your Trial</button> */}
+                <button class='beginButton' id='beginButton'>Begin Your Trial</button>
             </div>
         </div>
-    );
+  );
 }
 
-<<<<<<< HEAD
-function begin () {
-    // const clicked = document.getElementById('beginButton')
-    
-    // document.getElementById('beginButton').style.color = 'orange'
-    // onClick.console.log('oy')
-}
-export default Home;
-=======
+
+// const nextButton = document.getElementById('nextButton')
+
+
 
 export default Home;
-
->>>>>>> facd7696dc09c9ee4f1b56c3646896950d5a3842
